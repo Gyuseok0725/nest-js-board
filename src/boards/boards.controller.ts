b0fbc0b0fd9fa77675287fb5f1e58d1ec5c014f6
gyuseok25 <gyuseok25@gmail.com>
2021-10-11 15:59:33 +0900

    작업중

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardService: BoardsService){}
+    
+    @Get('/')
+    getAllBoard():Board[]{
+        return this.boardService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ):Board{
+        console.log('title: ',title);
+        console.log('description: ',description);
+
+        return this.boardService.createBoard(title, description);
+    }
+}
---
+++ src/boards/boards.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+import { Board, BoardStatus } from './board.model';
+
+
+@Injectable()
+export class BoardsService {
+    private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+
+    createBoard(title:string, description:string){
+        const board: Board = {
+            id:uuid(),
+            title,
+            description,
+            status:BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
+
+    }
+}
